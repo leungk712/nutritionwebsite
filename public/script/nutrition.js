9ce0089044523e1a0c0d28eac8574df3620a3040
Kevin Leung <leung.k.712@gmail.com>
2019-09-08 22:51:24 -0600

    update home page nutrition-facts

--- public/script/nutrition.js
+++ public/script/nutrition.js
@@ -1,1 +1,27 @@
+const nutritionFactsDiv = document.querySelector('nutrition-facts');
+const nutritionFactsContent = document.querySelector('.nutrition-facts-content');
+let dayInMilliseconds = 1000 * 60 * 60 * 24;
+
+const nutritionFactArray = [
+    'Did you know...',
+    'Hello...',
+    'Whatsup...',
+    'How are you?...',
+]
+
+function returnRandomFact(array){
+    let nutritionFact = Math.floor(Math.random() * array.length);
+    return array[nutritionFact];
+}
+
+function setIntervalAndExecute(fn, t) {
+    fn();
+    return(setInterval(fn, t));
+}
+
+setIntervalAndExecute(showFact, dayInMilliseconds);
+
+function showFact(){
+    nutritionFactsContent.textContent = returnRandomFact(nutritionFactArray);
+}
 
