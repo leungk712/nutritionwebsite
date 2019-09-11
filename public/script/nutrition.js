3fde416ce792ae949b2da41f05d9740d8ba43a23
Kevin Leung <leung.k.712@gmail.com>
2019-09-10 21:34:39 -0600

    update homepage nutrition-fact div

--- public/script/nutrition.js
+++ public/script/nutrition.js
@@ -1,25 +1,32 @@
 const nutritionFactsDiv = document.querySelector('nutrition-facts');
 const nutritionFactsContent = document.querySelector('.nutrition-facts-content');
 let dayInMilliseconds = 1000 * 60 * 60 * 24;
+let thirtySeconds = 1000 * 30;
 
 const nutritionFactArray = [
-    'Did you know...',
-    'Hello...',
-    'Whatsup...',
-    'How are you?...',
+    'It is a myth that all carbohydrates are unhealthy! ',
+    'Weight loss goes beyond just calories in and calories out. There are biochemical and psychological factors as well.',
+    '1 cup of chickpeas (garbanzo beans) contains about 14 grams of protein and 12 grams of fiber!',
+    'Antioxidants can be found in Vitamin C and Vitamin E foods.',
 ]
+
+/* Random generator from facts array */
 
 function returnRandomFact(array){
     let nutritionFact = Math.floor(Math.random() * array.length);
     return array[nutritionFact];
 }
 
-function setIntervalAndExecute(fn, t) {
+/* Set interval for random fact to display */
+
+function factInterval(fn, t) {
     fn();
     return(setInterval(fn, t));
 }
 
-setIntervalAndExecute(showFact, dayInMilliseconds);
+factInterval(showFact, thirtySeconds);
+
+/* Display fact in nutrition span */
 
 function showFact(){
     nutritionFactsContent.textContent = returnRandomFact(nutritionFactArray);
